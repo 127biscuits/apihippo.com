ed64596c80903ad2bdf189f9f15df0d5f9826d12
Álex González <alex.gonzalez@greenmangaming.com>
2014-10-19 23:10:24 +0100

    FE with Angular added
    The buttons are working, but it's ugly.

--- static/js/apihippo.js
+++ static/js/apihippo.js
@@ -1,13 +1,14 @@
 angular.module('hippos', [])
   .controller('HippoController', ['$scope', '$http', function($scope, $http){ 
     $scope.get = function () {
-      $http.get("http://random.apihippo.com:8000").success(function(data){
-        $scope.hippo = data
+      $http.get('http://random.apihippo.com:8000?verified=false').success(function(data){
+        data.url = data.url + "?height=480";
+        $scope.hippo = data;
       });
     };
 
     $scope.vote = function(hippoId) {
-      url = "http://apihippo.com:8000/" + hippoId + "/vote";
+      url = 'http://apihippo.com:8000/' + hippoId + '/vote';
       $http.post(url);
       // Don't bother about failures
       $scope.get();
